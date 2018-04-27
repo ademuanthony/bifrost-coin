c2a3fdd11236c9ea53362047611558d494e08cc4
Bifrost Actual <bifrost.actual@gmail.com>
2018-04-27 00:01:24 -0700

    Correct hover text style

--- src/clientversion.h
+++ src/clientversion.h
@@ -14,7 +14,7 @@
  */
 
 //! These need to be macros, as clientversion.cpp's and bifrost*-res.rc's voodoo requires it
-#define CLIENT_VERSION_MAJOR 0
+#define CLIENT_VERSION_MAJOR 1
 #define CLIENT_VERSION_MINOR 9
 #define CLIENT_VERSION_REVISION 1
 #define CLIENT_VERSION_BUILD 1
